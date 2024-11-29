eb207fd4c9a7a63d26cbd96a7ac97382ec6087d0
OrangeUnivers <118116610+OrangeUnivers@users.noreply.github.com>
2024-11-29 06:57:24 +0100

    Initial Commit

---
+++ data.js
@@ -0,0 +1,21 @@
+const defaultTable = [
+    [
+        {},
+        {subject: "Musik", room: "Altbau -19", teacher: "Daniel Leo Meier"},
+        {subject: "Sport", room: "Turnhalle A", teacher: "Urs Krüttli"},
+        {subject: "Chemie", room: "Neubau 109", teacher: "Eliane Fischer"},
+        {subject: "Deutsch", room: "Altbau 21", teacher: "Simon Spinner"}
+    ],[
+        {},
+        {subject: "Mathe", room: "Altbau 15", teacher: "Eliane Fischer"}
+    ],[
+        {},
+        {subject: "Mathe", room: "Altbau 15", teacher: "<span style='background-color: aqua; border-radius: 4px; padding-inline: 3px;''>Eliane Fischer</span>"}
+    ],[
+        {},
+        {subject: "Mathe", room: "Altbau 15", teacher: "<span style='background-color: aqua; border-radius: 4px; padding-inline: 3px;''>Eliane Fischer</span>"}
+    ],[
+        {},
+        {subject: "Mathe", room: "Altbau 15", teacher: "<span style='background-color: aqua; border-radius: 4px; padding-inline: 3px;''>Eliane Fischer</span>"}
+    ]
+];
