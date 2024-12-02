29bc1bd90ef21d3a65d67c52df4542dcbb301a08
OrangeUnivers <118116610+OrangeUnivers@users.noreply.github.com>
2024-12-02 07:56:41 +0100

    Add files via upload

--- data.js
+++ data.js
@@ -5,17 +5,51 @@ const defaultTable = [
         {subject: "Sport", room: "Turnhalle A", teacher: "Urs Krüttli"},
         {subject: "Chemie", room: "Neubau 109", teacher: "Eliane Fischer"},
         {subject: "Deutsch", room: "Altbau 21", teacher: "Simon Spinner"}
-    ],[
+    ], [
+        {subject: "Mathe", room: "Altbau 15", teacher: "Mattia Rutishauser"},
+        {subject: "Französisch", room: "Altbau 16", teacher: "Elisabeth Frey-Martins"},
+        {subject: "Sport", room: "Turnhalle A", teacher: "Urs Krüttli"},
+        {subject: "Chemie", room: "Neubau 109", teacher: "Eliane Fischer"},
+        {subject: "Deutsch", room: "Altbau 21", teacher: "Simon Spinner"}
+    ], [
+        {subject: "Mathe", room: "Altbau 15", teacher: "Mattia Rutishauser"},
+        {subject: "Latein", room: "Altbau 14", teacher: "Johannes Comiotto"},
+        {subject: "Deutsch", room: "Altbau 11", teacher: "Simon Spinner"},
+        {subject: "Geschichte", room: "Neubau 004", teacher: "Marcel Jäger"},
+        {subject: "Sport", room: "Turnhalle B", teacher: "Urs Krüttli"}
+    ], [
+        {subject: "Französisch", room: "Altbau 16", teacher: "Elisabeth Frey-Martins"},
+        {subject: "Informatik", room: "Altbau 04", teacher: "Andreas Schaufelbühl"},
+        {subject: "Bildnerisches Gestalten", room: "Altbau 37", teacher: "Larissa Troxler"},
+        {subject: "Deutsch", room: "Altbau 21", teacher: "Simon Spinner"},
+        {subject: "Geschichte", room: "Neubau 004", teacher: "Marcel Jäger"}
+    ], [
+        {subject: "Latein", room: "Altbau 14", teacher: "Johannes Comiotto"},
+        {subject: "Englisch", room: "Kirchgemeindehaus 1", teacher: "Patrick Schetters"},
+        {subject: "Bildnerisches Gestalten", room: "Altbau 37", teacher: "Larissa Troxler"},
+        {subject: "Französisch", room: "Altbau 13", teacher: "Elisabeth Frey-Martins"},
+        {subject: "Biologie", room: "Neubau 007", teacher: "Marc Eger"}
+    ], [
+        {subject: "Englisch", room: "Altbau 25", teacher: "Patrick Schetters"},
+        {subject: "Religion, Kultur und Ethik", room: "Neubau 404", teacher: "Michel Borner"},
+        {subject: "Instrumentalunterricht", room: "Kirchgemeindehaus 0.15", teacher: "Florian Reichle"},
+        {subject: "Geographie", room: "Neubau 011", teacher: "Souria Nussbaumer"},
+        {subject: "Geographie", room: "Neubau 011", teacher: "Souria Nussbaumer"}
+    ], [
+        {subject: "Englisch", room: "Altbau 25", teacher: "Patrick Schetters"},
+        {subject: "Mathe", room: "Altbau 15", teacher: "Mattia Rutishauser"},
         {},
-        {subject: "Mathe", room: "Altbau 15", teacher: "Eliane Fischer"}
-    ],[
+        {subject: "Mathe", room: "Altbau 15", teacher: "Mattia Rutishauser"},
+        {subject: "Musik", room: "Kirchgemeindehaus MFR", teacher: "Daniel Leo Meier"}
+    ], [
+        {subject: "Biologie", room: "Neubau 008", teacher: "Marc Eger"},
         {},
-        {subject: "Mathe", room: "Altbau 15", teacher: "<span style='background-color: aqua; border-radius: 4px; padding-inline: 3px;''>Eliane Fischer</span>"}
-    ],[
         {},
-        {subject: "Mathe", room: "Altbau 15", teacher: "<span style='background-color: aqua; border-radius: 4px; padding-inline: 3px;''>Eliane Fischer</span>"}
-    ],[
-        {},
-        {subject: "Mathe", room: "Altbau 15", teacher: "<span style='background-color: aqua; border-radius: 4px; padding-inline: 3px;''>Eliane Fischer</span>"}
+        {subject: "Latein", room: "Altbau 15", teacher: "Johannes Comiotto"}
+    ], [
+        {subject: "Klassenstunde", room: "Neubau 009", teacher: "Souria Nussbaumer"}
     ]
 ];
+const subjectList = {"Mathe": "math", "Französisch": "french", "Latein": "latin", "Englisch": "english", "Biologie": "biologie", "Klassenstunde": "classhour", "Musik": "music", "Informatik": "informatics", "Religion, Kultur und Ethik": "rke",
+    "Sport": "sports", "Deutsch": "german", "Bildnerisches Gestalten": "bg", "Instrumentalunterricht": "iu", "Chemie": "chemistry", "Geschichte": "history", "Geographie": "geography"};
+const subjectArray = Object.keys(subjectList);
